1d3baec3fa0ca8466450247e3f556da5f02f9b39
Zackory <zackory@gatech.edu>
2019-10-08 18:35:12 -0400

    Reverting

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='pytorch_ppo',
+setup(name='a2c_ppo_acktr',
       packages=find_packages(),
       version='0.0.1',
       install_requires=['gym', 'matplotlib', 'pybullet'])
