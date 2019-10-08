6380fc592a88682a39730567ca4b1fab97459ad0
Zackory <zackory@gatech.edu>
2019-10-08 19:43:05 -0400

    Installing a2c_ppo_acktr

--- setup.py
+++ setup.py
@@ -4,3 +4,9 @@ setup(name='pytorch_ppo',
       packages=find_packages(),
       version='0.1.0',
       install_requires=['gym', 'matplotlib', 'pybullet', 'torch', 'torchvision'])
+
+setup(name='a2c_ppo_acktr',
+      packages=find_packages('pytorch_ppo'),
+      version='0.1.0',
+      install_requires=['gym', 'matplotlib', 'pybullet', 'torch', 'torchvision'])
+
