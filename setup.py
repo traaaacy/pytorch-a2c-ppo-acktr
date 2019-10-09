ece000bfa1841477aeb6c49f9c31b993eb54cc23
Zackory <zackory@gatech.edu>
2019-10-08 20:07:45 -0400

    Setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(name='a2c_ppo_acktr',
-      packages=find_packages('pytorch_ppo'),
-      package_dir={'': 'pytorch_ppo'},
+      packages=find_packages('ppo'),
+      package_dir={'': 'ppo'},
       version='0.1.0',
       install_requires=['gym', 'matplotlib', 'pybullet', 'torch', 'torchvision'])
 
