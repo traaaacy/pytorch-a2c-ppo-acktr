5471ccefbf8b3b143b00b228bc1c69658dee86df
Zackory Erickson <zackory@gatech.edu>
2020-03-22 19:36:25 -0400

    Correct baselines requirement version

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(name='a2c_ppo_acktr',
       packages=find_packages('ppo'),
       package_dir={'': 'ppo'},
       version='0.1.0',
-      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines'])
+      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines @ git+https://github.com/openai/baselines'])
 
 setup(name='ppo',
       packages=find_packages(),
       version='0.1.0',
-      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines'])
+      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines @ git+https://github.com/openai/baselines'])
