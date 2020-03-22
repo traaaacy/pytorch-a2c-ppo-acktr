a3ff023db318b6f3595c8ac67c62e266bf96434e
Zackory Erickson <zackory@gatech.edu>
2020-03-22 19:26:43 -0400

    Updating requirements

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(name='a2c_ppo_acktr',
       packages=find_packages('ppo'),
       package_dir={'': 'ppo'},
       version='0.1.0',
-      install_requires=['gym', 'matplotlib', 'assistive-gym', 'torch', 'torchvision', 'baselines'])
+      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines'])
 
 setup(name='ppo',
       packages=find_packages(),
       version='0.1.0',
-      install_requires=['gym', 'matplotlib', 'assistive-gym', 'torch', 'torchvision', 'baselines'])
+      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines'])
