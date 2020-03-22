d2b25aec45e69db8e5d72159be098a5f64afba75
Zackory Erickson <zackory@gatech.edu>
2020-03-22 19:49:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(name='a2c_ppo_acktr',
       packages=find_packages('ppo'),
       package_dir={'': 'ppo'},
       version='0.1.0',
-      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines==0.1.6 @ git+https://github.com/openai/baselines'])
+      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines @ git+https://github.com/openai/baselines#egg=baselines-0.1.6'])
 
 setup(name='ppo',
       packages=find_packages(),
       version='0.1.0',
-      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines==0.1.6 @ git+https://github.com/openai/baselines'])
+      install_requires=['gym', 'matplotlib', 'torch', 'torchvision', 'baselines @ git+https://github.com/openai/baselines#egg=baselines-0.1.6'])
