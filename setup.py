f54a9488b1030cf5fb151ce79046e2a3cd8a6921
Zackory Erickson <zackory@gatech.edu>
2020-03-22 19:25:20 -0400

    Adding assistive-gym requirement

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ setup(name='a2c_ppo_acktr',
       packages=find_packages('ppo'),
       package_dir={'': 'ppo'},
       version='0.1.0',
-      install_requires=['gym', 'matplotlib', 'pybullet', 'torch', 'torchvision', 'baselines'])
+      install_requires=['gym', 'matplotlib', 'assistive-gym', 'torch', 'torchvision', 'baselines'])
 
 setup(name='ppo',
       packages=find_packages(),
       version='0.1.0',
-      install_requires=['gym', 'matplotlib', 'pybullet', 'torch', 'torchvision', 'baselines'])
-
+      install_requires=['gym', 'matplotlib', 'assistive-gym', 'torch', 'torchvision', 'baselines'])
