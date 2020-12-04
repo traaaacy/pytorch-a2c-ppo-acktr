92a178eda847889df409b40196bd5eb13f94335d
Zackory Erickson <zackory@gatech.edu>
2020-12-04 17:35:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup, find_packages
 setup(name='ppo',
       packages=find_packages(),
       version='0.1.0',
-      install_requires=['gym', 'matplotlib', 'torch==1.5.0', 'torchvision==0.6.0', 'baselines @ git+git://github.com/openai/baselines.git@master#egg=baselines'])
+      # install_requires=['gym', 'matplotlib', 'torch==1.5.0', 'torchvision==0.6.0', 'baselines @ git+git://github.com/openai/baselines.git@master#egg=baselines'])
+      install_requires=['gym', 'matplotlib', 'torch==1.5.0', 'torchvision==0.6.0', 'baselines @ git+https://github.com/openai/baselines.git#egg=baselines'])
