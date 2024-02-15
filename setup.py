28f33b8e7848ad7b11d2098368e92c79d860dca1
traaaacy <109930793+traaaacy@users.noreply.github.com>
2024-02-15 21:23:22 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name='ppo',
       packages=find_packages(),
       version='0.1.0',
       # install_requires=['gym', 'matplotlib', 'torch==1.5.0', 'torchvision==0.6.0', 'baselines @ git+git://github.com/openai/baselines.git@master#egg=baselines'])
-      install_requires=['gym', 'matplotlib', 'tensorflow', 'torch==1.5.0', 'torchvision==0.6.0', 'baselines @ git+https://github.com/openai/baselines.git#egg=baselines'])
+      install_requires=['gym', 'matplotlib',  'torchvision==0.6.0', 'baselines @ git+https://github.com/openai/baselines.git#egg=baselines'])
